b1057ce0f42d11d186d7b57fbb1a0b7ac77e513e
jyrki096 <jyrki096@gmail.com>
2022-06-03 21:28:54 +0300

    Добавлен класс ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -3,7 +3,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators():
-    #LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    MAIN_TITLE = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    MAIN_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main p")
+    BASKET_TITLE = (By.CSS_SELECTOR, ".alertinner strong")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+
