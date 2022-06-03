72559c8961381717866d5f8b33c3451cbd6db3bb
jyrki096 <jyrki096@gmail.com>
2022-06-03 21:25:24 +0300

    Добавлен файл с тест кейсом добавления товара в корзину

---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+from .pages.product_page import ProductPage
+
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_right_titles()
+    product_page.should_be_right_prices()
