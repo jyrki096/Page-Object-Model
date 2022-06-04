3f33ac44dc6713f63a3ca4e8a7e69f30223675f1
jyrki096 <jyrki096@gmail.com>
2022-06-04 10:18:55 +0300

    - Добавлены тест кейсы для отрицательных проверок, такие как "test_guest_cant_see_success_message_after_adding_product_to_basket", "test_guest_cant_see_success_message" и "test_message_disappeared_after_adding_product_to_basket".

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
-@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+'''@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -20,4 +20,24 @@ def test_guest_can_add_product_to_basket(browser, links):
     product_page.should_be_add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_right_titles()
-    product_page.should_be_right_prices()
+    product_page.should_be_right_prices()'''
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket()
+    page.should_disappear_success_message()
