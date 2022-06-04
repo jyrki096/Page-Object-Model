abaad0c72f8c9cd6e9fc3be75919de36ce01c2c9
jyrki096 <jyrki096@gmail.com>
2022-06-04 11:11:32 +0300

    Добавлены тест кейсы "test_guest_should_see_login_link_on_product_page" и "test_guest_can_go_to_login_page_from_product_page".

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
-'''@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -20,7 +20,7 @@ def test_guest_can_add_product_to_basket(browser, links):
     product_page.should_be_add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_right_titles()
-    product_page.should_be_right_prices()'''
+    product_page.should_be_right_prices()
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -41,3 +41,16 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.should_be_add_to_basket()
     page.should_disappear_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
