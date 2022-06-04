0ab7817e4c95ee9539c84d3536e2b256a4918df5
jyrki096 <jyrki096@gmail.com>
2022-06-04 13:16:35 +0300

    Удалён импорт 'from selenium.webdriver.common.by import By'

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.webdriver.common.by import By
+
 
 class ProductPage(BasePage):
     def should_be_add_to_basket(self):
