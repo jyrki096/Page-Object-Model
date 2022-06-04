aa4af845fa7062dd7c26f1f15abe99e826fd0873
jyrki096 <jyrki096@gmail.com>
2022-06-04 16:39:12 +0300

    Добавлен класс TestUserAddToBasketFromProductPage c кейсами регистрации пользователя и работы с корзиной для авторизированного пользователя

--- test_product_page.py
+++ test_product_page.py
@@ -32,7 +32,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_add_to_basket()
     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
@@ -68,3 +67,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.should_be_empty_basket()
     page.should_be_basket_empty_message()
+
+class  TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        self.login_page = LoginPage(browser, link)
+        self.login_page.open()
+        count = random.randint(1, 100)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time() + count)
+        self.login_page.register_new_user(email, password)
+        self.login_page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        self.product_page = ProductPage(browser, link)
+        self.product_page.open()
+        self.product_page.should_be_add_to_basket()
+        #product_page.solve_quiz_and_get_code()
+        self.product_page.should_be_right_titles()
+        self.product_page.should_be_right_prices()
+
+    def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        self.page = ProductPage(browser, link)
+        self.page.open()
+        self.page.should_not_be_success_message()
