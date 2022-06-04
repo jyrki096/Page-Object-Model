119d1769b090988dc0bfd41ec9f441ce9b74d3f9
jyrki096 <jyrki096@gmail.com>
2022-06-04 13:12:08 +0300

    Добавлен тест кейс " test_guest_cant_see_product_in_basket_opened_from_product_page"

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,9 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import pytest
+import random
+import time
 
 @pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -22,6 +26,8 @@ def test_guest_can_add_product_to_basket(browser, links):
     product_page.should_be_right_titles()
     product_page.should_be_right_prices()
 
+
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -35,6 +41,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -54,3 +61,10 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_be_empty_basket()
+    page.should_be_basket_empty_message()
