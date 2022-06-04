db56d108e9d817c79e6ad05c5f50c6cffc3b7882
jyrki096 <jyrki096@gmail.com>
2022-06-04 16:35:28 +0300

    Добавлен метод регистрации нового пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from selenium.webdriver.common.by import By
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -16,3 +16,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form has not found"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_first = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
+        password_first.send_keys(password)
+        password_repeat = self.browser.find_element(*LoginPageLocators.PASSWORD_REPEAT_FIELD)
+        password_repeat.send_keys(password)
+        button_submit = self.browser.find_element(*LoginPageLocators.SUBMIT_BUTTON)
+        button_submit.click()
+
